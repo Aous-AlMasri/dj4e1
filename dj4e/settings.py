782ee6a2a5c940c0768387aefa5c665b2e7af82e
Aous-AlMasri <112282362+Aous-AlMasri@users.noreply.github.com>
2024-12-29 21:44:29 +0300

    Update settings.py add if not DEBUG (in production only)

--- dj4e/settings.py
+++ dj4e/settings.py
@@ -136,6 +136,12 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "home/static")
 
+if not DEBUG:  # Ensure this only runs in production
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = []
+    STATIC_ROOT = os.path.join(BASE_DIR, "home/static")
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
